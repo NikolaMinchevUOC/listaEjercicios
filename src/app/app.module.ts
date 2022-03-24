63ca4924ed85fd55c43c5992f046343fef244323
NikolaMinchevUOC <nminchev@uoc.edu>
2022-03-24 13:39:56 +0100

    Añadido componente detalle-ejercicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EjerciciosComponent } from './ejercicios/ejercicios.component';
+import { EjercicioDetalleComponent } from './ejercicio-detalle/ejercicio-detalle.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, EjerciciosComponent, EjercicioDetalleComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
